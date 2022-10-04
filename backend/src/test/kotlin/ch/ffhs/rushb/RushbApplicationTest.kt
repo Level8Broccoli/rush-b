de176fa09b0c65526667594ccffd8aa194094e2b
lpapailiou <lena@kaiki.ch>
2022-10-04 20:51:54 +0200

    docker DEV running for Lena :)

---
+++ backend/src/test/kotlin/ch/ffhs/rushb/RushbApplicationTest.kt
@@ -0,0 +1,13 @@
+package ch.ffhs.rushb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RushbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
